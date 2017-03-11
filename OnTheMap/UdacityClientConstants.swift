eacef4c489d64587f2b73c35c0090e23f18b13f8
Austin Tooley <austin@tooley.me>
2017-03-11 14:03:22 -0600

    Init

---
+++ OnTheMap/UdacityClientConstants.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityClientConstants.swift
+//  OnTheMap
+//
+//  Created by Austin Tooley on 3/11/17.
+//  Copyright © 2017 Austin Tooley. All rights reserved.
+//
+
+// MARK:  - UdacityClient (Constants)
