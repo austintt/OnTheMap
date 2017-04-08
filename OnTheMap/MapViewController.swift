2bf0f64e07f4b14f659aee36d84fe8c7b2855ddf
Austin Tooley <austin@tooley.me>
2017-04-08 13:12:30 -0500

    Annotations now launch url

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -12,12 +12,14 @@ import UIKit
 import MapKit
 
 
-class MapViewController: UIViewController, UINavigationControllerDelegate {
+class MapViewController: UIViewController, UINavigationControllerDelegate, MKMapViewDelegate {
 
-    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
         
         // Get 100 pins and display
         let params = [ServiceManager.ParemeterKeys.Limit: 100, ServiceManager.ParemeterKeys.Skip: 0]
@@ -52,18 +54,74 @@ class MapViewController: UIViewController, UINavigationControllerDelegate {
     }
     
     // Pins
+//    func addPins(locations: [Location]) {
+//        
+//        for location in locations {
+//            let pin = MKPointAnnotation()
+//            if let lat = location.latitude, let long = location.longitude, let first = location.firstName, let last = location.lastName, let url = location.mediaURL {
+//                // Set annotation data
+//                pin.title = "\(first) \(last)"
+//                pin.subtitle = url
+//                pin.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+//        
+//                
+//                // Add the pin
+//                performUIUpdatesOnMain {
+//                    
+//                    self.map.addAnnotation(pin)
+//                }
+//            }
+//        }
+//    }
+    
     func addPins(locations: [Location]) {
         
-        for location in locations {
-            let pin = MKPointAnnotation()
-            if let lat = location.latitude, let long = location.longitude {
-                pin.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
-                performUIUpdatesOnMain {
-                    self.map.addAnnotation(pin)
-                }
+        var annotations = [MKPointAnnotation]()
+        
+        for location in locations  {
+            // Make annotation
+            if let lat = location.latitude, let long = location.longitude, let first = location.firstName, let last = location.lastName, let url = location.mediaURL {
+                let annotation = MKPointAnnotation()
+                annotation.title = "\(first) \(last)"
+                annotation.subtitle = url
+                annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                
+                // Add to list
+                annotations.append(annotation)
+            }
+            performUIUpdatesOnMain {
+                self.mapView.addAnnotations(annotations)
             }
         }
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let id = "pin"
+        var pinView = MKPinAnnotationView()
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: id) as? MKPinAnnotationView {
+            dequeuedView.annotation = annotation
+            pinView = dequeuedView
+        } else {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            pinView.canShowCallout = true
+            pinView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let url = view.annotation?.subtitle! {
+                app.openURL(NSURL(string: url)! as URL)
+            }
+        }
+    }
+
     
     // Refresh
     
